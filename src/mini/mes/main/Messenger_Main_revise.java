3388dcdf80be5bfea859925b97929963a1221920
user <user@DESKTOP-7EQF3M8>
2018-08-29 14:18:30 +0900

    올라갑니다

--- src/mini/mes/main/Messenger_Main_revise.java
+++ src/mini/mes/main/Messenger_Main_revise.java
@@ -5,18 +5,18 @@ import java.awt.event.*;
 import javax.swing.*;
 
 /**
- *	메신저 리스트 Test 입니다.
+ *	Swing에서 사용하는 Frame : JFrame 
  */
-class FriendList extends JFrame{
+class Window01 extends JFrame{
 	
 //	컴포넌트를 배치할 영역을 JPanel로 구현
 	private JPanel con = new JPanel();
 	
+	private JTabbedPane tab = new JTabbedPane(JTabbedPane.TOP);
 	
 	
 	
-	
-	public FriendList() {
+	public Window01() {
 		this.display();
 		this.event();
 		this.menu();
@@ -33,21 +33,22 @@ class FriendList extends JFrame{
 	 */
 	public void display() {
 		this.setContentPane(con);//con을 Component 설정 영역으로 등록
-
+		
+		this.getContentPane().add(tab, BorderLayout.CENTER);
+		
 	}
 	/**
 	 * 이벤트 설정 메소드
 	 */
 	public void event() {
-		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);//x누르면 창 소멸
-		WindowListener closer = new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				int close = JOptionPane.showConfirmDialog(con, "종료하시겠습니까?", "종료", JOptionPane.YES_NO_OPTION);
-				if(close == 0) System.exit(0);
-			}
-		};
-		this.addWindowListener(closer);	
+//		awt에서는 WindowEvent를 직접 구현해서 x버튼을 처리
+//		 -> Swing에서는 옵션을 통해 지정하도록 변경
+//		this.setDefaultCloseOperation(EXIT_ON_CLOSE);//x누르면 프로그램 종료
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);//x누르면 창 소멸
+//		this.setDefaultCloseOperation(HIDE_ON_CLOSE);//x누르면 창 숨김
+//		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);//사용자 직접 구현
+		
+		
 	}
 	/**
 	 * 메뉴 구현 메소드
@@ -55,13 +56,11 @@ class FriendList extends JFrame{
 	public void menu() {
 		
 	}
+	
 }
 
-public class Messenger_List {
+public class Messenger_Main_revise {
 	public static void main(String[] args) {
-		List window = new List();
-		
-
-		
+		Window01 window = new Window01();
 	}
 }
