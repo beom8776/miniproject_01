cf560543148e3fbacf0a61a9c6debd90371411b1
user <user@DESKTOP-7EQF3M8>
2018-08-31 09:05:27 +0900

    메인화면 작업중 입니다.

---
+++ src/mini/mes/main/Messenger_FriendAdd.java
@@ -0,0 +1,127 @@
+package mini.mes.main;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+/**
+ * 친구찾기 / 추가 Tab
+ * @author 김현진
+ *
+ */
+public class Messenger_FriendAdd extends JPanel{
+
+	/**
+	 * 친구 검색 영역
+	 */
+	private JPanel searchPan = new JPanel();
+	private JLabel titlelb = new JLabel("친구를 찾아보세요");
+	private JTextField searchFd = new JTextField();
+	private JLabel searchtitle = new JLabel("친구 ID");
+	private JButton searchBt = new JButton("검색");
+	private JLabel label = new JLabel("검색결과");
+
+	/**
+	 * 검색 결과 영역
+	 */
+	private JPanel resultPan = new JPanel();
+	
+	
+//	친구 검색 결과 출력 (별도 클래스 필요)
+	private JPanel hasResult = new JPanel();
+	private JLabel noResult = new JLabel("검색결과가 없습니다.");
+	private JLabel searpicture1 = new JLabel("사진");
+	private JLabel searname1 = new JLabel("이름");
+	private JLabel searment = new JLabel("상태메시지");
+	private JButton searadd = new JButton("추가");
+
+	public Messenger_FriendAdd() {
+		this.event();
+		
+		/**
+		 * 친구찾기 영역 패널
+		 */
+		this.setLayout(null);	
+		this.add(searchPan);
+		searchPan.setBounds(0, 0, 378, 200);
+		searchPan.setLayout(null);
+		
+		titlelb.setFont(new Font("굴림", Font.BOLD, 14));
+		titlelb.setHorizontalAlignment(SwingConstants.CENTER);
+		searchPan.add(titlelb);
+		titlelb.setBounds(122, 20, 140, 30);
+		
+		searchFd.setBounds(102, 60, 180, 22);
+		searchPan.add(searchFd);
+		searchFd.setColumns(20);
+		
+		searchtitle.setFont(new Font("굴림", Font.BOLD, 12));
+		searchPan.add(searchtitle);
+		searchtitle.setBounds(45, 60, 45, 22);
+		
+		searchBt.setFont(new Font("굴림", Font.BOLD, 12));
+		searchPan.add(searchBt);
+		searchBt.setBounds(222, 92, 60, 23);
+		
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("굴림", Font.BOLD, 12));
+		searchPan.add(label);
+		label.setBounds(122, 176, 140, 19);
+		
+		/**
+		 * 검색결과영역 패널
+		 */
+		resultPan.setLayout(null);
+		this.add(resultPan);
+		resultPan.setBounds(1, 200, 378, 533); //382 / 560
+		resultPan.setBorder(new LineBorder(Color.ORANGE, 2));
+		
+		/**
+		 * 검색결과 없음 출력
+		 */
+		noResult.setEnabled(false);
+		noResult.setVisible(true);
+		noResult.setFont(new Font("굴림", Font.BOLD, 12));
+		noResult.setHorizontalAlignment(SwingConstants.CENTER);
+		resultPan.add(noResult);
+		noResult.setBounds(102, 268, 180, 25);
+		
+		/**
+		 * 검색 후 결과 출력(별도 클래스 필요)
+		 */
+		hasResult.setLayout(null);
+		resultPan.add(hasResult);
+		hasResult.setBounds(2, 2, 370, 60);
+		
+		searpicture1.setHorizontalAlignment(SwingConstants.CENTER);
+		searpicture1.setBounds(1, 1, 57, 57);
+		hasResult.add(searpicture1);
+		
+		searname1.setBounds(70, 3, 160, 25);
+		hasResult.add(searname1);
+		
+		searment.setBounds(70, 30, 230, 25);
+		hasResult.add(searment);
+		
+		searadd.setBounds(298, 18, 72, 24);
+		hasResult.add(searadd);
+		searadd.setFont(new Font("굴림", Font.BOLD, 13));
+	}
+	public void event() {
+		
+		/**
+		 * 검색버튼 이벤트
+		 */
+		searchBt.addActionListener(e->{
+//			회원DB에서 검색한 ID또는 이름을 찾는다.
+//			그리고 회원이 등록한 사진, 이름, 상태메시지를 Panel에 담는다.
+			
+		});
+	}
+}
+
+
+
+
+
--- src/mini/mes/main/Messenger_List.java
+++ src/mini/mes/main/Messenger_List.java
@@ -3,65 +3,73 @@ package mini.mes.main;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 /**
- *	메신저 리스트 Test 입니다.
+ * 친구목록 Tab
+ * @author 김현진
+ *
  */
-class FriendList extends JFrame{
-	
-//	컴포넌트를 배치할 영역을 JPanel로 구현
-	private JPanel con = new JPanel();
+class Messenger_List extends JPanel{
 	
 	
+	private JPanel frList = new JPanel();
+	private JPanel friend1 = new JPanel();
 	
+	private ImageIcon fricon1 = new ImageIcon("D:\\\\Java\\\\workspace\\\\Image_group\\\\NoIMG.jpg");
+	private JLabel frpictur1 = new JLabel(fricon1);
+	private JLabel frname1 = new JLabel("홍길동");
+	private JLabel frment1 = new JLabel("내가 누구죠? 그게 정말 너무 궁금해요~~~ 알죠?");
+	private JButton startfr1 = new JButton("1:1대화");
 	
+	private JPanel friend2 = new JPanel();
 	
-	public FriendList() {
-		this.display();
-		this.event();
-		this.menu();
-		
-		this.setTitle("Java Messenger");
-//		this.setLocation(100, 100);
-		this.setLocationByPlatform(true);
-		this.setSize(400, 800);
-		this.setResizable(false);
-		this.setVisible(true);
-	}
-	/**
-	 * 화면 구현 메소드
-	 */
-	public void display() {
-		this.setContentPane(con);//con을 Component 설정 영역으로 등록
+	private ImageIcon fricon2 = new ImageIcon("D:\\\\Java\\\\workspace\\\\Image_group\\\\NoIMG.jpg");
+	private JLabel frpictur2 = new JLabel(fricon2);
+	private JLabel frname2 = new JLabel("홍진영");
+	private JLabel frment2 = new JLabel("네네네네네 배터리");
+	private JButton startfr2 = new JButton("1:1대화");
 
-	}
-	/**
-	 * 이벤트 설정 메소드
-	 */
-	public void event() {
-		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);//x누르면 창 소멸
-		WindowListener closer = new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				int close = JOptionPane.showConfirmDialog(con, "종료하시겠습니까?", "종료", JOptionPane.YES_NO_OPTION);
-				if(close == 0) System.exit(0);
-			}
-		};
-		this.addWindowListener(closer);	
-	}
-	/**
-	 * 메뉴 구현 메소드
-	 */
-	public void menu() {
+	
+	public Messenger_List() {
+		this.setLayout(null);
 		
-	}
-}
-
-public class Messenger_List {
-	public static void main(String[] args) {
-		List window = new List();
+		/**
+		 * 친구리스트영역 패널
+		 */
+		this.add(frList);
+		frList.setLayout(null);
+		frList.setBounds(1, 1, 378, 732);
 		
-
+		frList.add(friend1);
+		friend1.setBounds(1, 1, 376, 60); // X, Y, width, height
+		friend1.setLayout(null);
+		friend1.setBorder(new LineBorder(Color.ORANGE, 1));
 		
+//		친구 추가에 대한 별도 클래스 필요
+		friend1.add(frpictur1);
+		frpictur1.setBounds(1, 1, 57, 57);
+		friend1.add(frname1);
+		frname1.setBounds(70, 3, 160, 25);
+		friend1.add(frment1);
+		frment1.setBounds(70, 30, 230, 25);
+		friend1.add(startfr1);
+		startfr1.setBounds(298, 18, 72, 24);
+		startfr1.setFont(new Font("굴림", Font.BOLD, 9));
+		
+		frList.add(friend2);
+		friend2.setBounds(1, 65, 376, 60); // X, Y, width, height
+		friend2.setLayout(null);
+		friend2.setBorder(new LineBorder(Color.ORANGE, 1));
+		
+		friend2.add(frpictur2);
+		frpictur2.setBounds(1, 1, 57, 57);
+		friend2.add(frname2);
+		frname2.setBounds(70, 3, 160, 25);
+		friend2.add(frment2);
+		frment2.setBounds(70, 30, 230, 25);
+		friend2.add(startfr2);
+		startfr2.setBounds(298, 18, 72, 24);
+		startfr2.setFont(new Font("굴림", Font.BOLD, 9));
 	}
 }
